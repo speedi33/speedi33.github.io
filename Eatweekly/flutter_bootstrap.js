d80ad7b4e9dc43232284e3efaa90ab8587245748
speedi33 <hohm.lukas@gmail.com>
2026-08-14 13:35:47 +0200

    Deploy Eatweekly: UI/UX overhaul (M3 theme, redesigned pages)

--- Eatweekly/flutter_bootstrap.js
+++ Eatweekly/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"ef0cd000916d64fa0c5d09cc809fa7ad244a57
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "992745776"
+    serviceWorkerVersion: "1605895612"
   }
 });
